4c3b8523260370f9f07188c58bfafc656bd6a7d6
97RIS <pi04r2189@gmail.com>
2024-09-16 09:53:15 +0100

    Add files via upload

---
+++ musicahangul.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+hangul_text = "안녕하세요! Hello in Hangul."
+print(hangul_text)
+print("")
+letraMusica="""어느 날 세상이 멈췄어, 아무런 예고도 하나 없이
+봄은 기다림을 몰라서, 눈치 없이 와버렸어
+발자국이 지워진 거리, 여기 넘어져 있는 나
+혼자 가네 시간이, 미안해 말도 없이, yeah"""
+
+print(letraMusica)
+print("")
+
+print("안녕하세요, 세계!")
